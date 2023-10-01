659ec64f4fdc7d722028bd27445e61805223de46
Ryan_Bowens <bowensryan@gmail.com>
2023-10-01 14:18:03 -0700

    BankAccounts Assignment

--- BankAccounts/BankAccounts.py
+++ BankAccounts/BankAccounts.py
@@ -1,60 +1,34 @@
 class BankAccount:
     def __init__(self, int_rate, balance): 
-        self.int_rate = 0.02
-        self.balance = 100
+        self.int_rate = int_rate
+        self.balance = balance
+        self.interest = 0
 
     def deposit(self, amount):
+        print(f"Deposited: ${amount}")
         self.balance += amount
+        # print(self.balance)
+        return self
 
     def withdraw(self, amount):
+        print(f"Withdrew: ${amount}")
         self.balance -= amount
         if (self.balance < amount):
             print("Insufficient Funds: Charging a $5 fee")
             self.balance -= 5
-
-    def display_account_info(self):
-        print(self.balance)
-
-    def yield_interest(self):
-        self.balance = self.balance * self.int_rate
         return self
 
-class User:
-    def __init__(self, fName, lName, email, age):
-        self.fName = fName
-        self.lName = lName
-        self.email = email
-        self.age = age
-        self.is_rewards_member = False
-        self.gold_card_points = 0
-        self.account = BankAccount(int_rate = 0.02, balance = 0)
-
-    def display_info(self):
-        print(self.fName, self.lName, self.email, self.age)
-
-    def enroll(self):
-        self.is_rewards_member = True
-        self.gold_card_points = 200
-
-    def spend_points(self, amount):
-        self.gold_card_points -= amount
+    def yield_interest(self):
+        self.interest = self.balance * self.int_rate
+        return self
     
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
+    def display_account_info(self):
+        print(f"Your Balance: {self.balance}, Your Interest: {self.interest}")
+        return self
+    
+ba1 = BankAccount(0.02, 100)
+ba2 = BankAccount(0.02, 200)
 
-    def display_user_balance(self):
-        self.account.display_account_info()
-
-
-c1 = User("Ryan", "Bowens", "bowensryan@gmail.com", 25)
-
-c1.display_info()
-c1.enroll()
-print(c1.is_rewards_member, c1.gold_card_points)
-c1.spend_points(20)
-print(f"Remaining points: {c1.gold_card_points}")
-
-c1.make_deposit(50)
-c1.account.display_account_info()
-
-ba1 = BankAccount
+ba1.display_account_info().deposit(50).deposit(50).deposit(50).withdraw(100).yield_interest().display_account_info()
+print("----------")
+ba2.display_account_info().deposit(25).deposit(75).withdraw(150).withdraw(50).withdraw(100).withdraw(80).yield_interest().display_account_info()
